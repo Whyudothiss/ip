6c7b4e2966a08399413bb330e4277b6bfafd6290
Whyudothiss <weichenhow@gmail.com>
2025-08-19 00:06:56 +0800

    added borders between sentences

--- src/main/java/GiChat.java
+++ src/main/java/GiChat.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 public class GiChat {
     public static void main(String[] args) {
+        String border = "______________________________________________";
+        System.out.println(border);
         System.out.println("Hello I'm your GiChat \nWhat can I do for you");
+        System.out.println(border);
 
         Scanner scanner = new Scanner(System.in);
         String line;
 
         // Scans the next line of input and check if the user says "bye"
         while (!(line = scanner.nextLine()).equals("bye")) {
+            System.out.println(border);
             System.out.println(line);
+            System.out.println(border);
         }
-
+        System.out.println(border);
         System.out.println("Bye. Hope to see you again!");
+        System.out.println(border);
     }
 }

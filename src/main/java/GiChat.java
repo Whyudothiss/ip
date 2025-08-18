570d4bf8843ed302c352382dc5054ce2ef944ab7
Whyudothiss <weichenhow@gmail.com>
2025-08-19 00:00:53 +0800

    Added scanner function
    Added scanner to scan for user input and check if user says bye or not

--- src/main/java/GiChat.java
+++ src/main/java/GiChat.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class GiChat {
     public static void main(String[] args) {
         System.out.println("Hello I'm your GiChat \nWhat can I do for you");
+
+        Scanner scanner = new Scanner(System.in);
+        String line;
+
+        // Scans the next line of input and check if the user says "bye"
+        while (!(line = scanner.nextLine()).equals("bye")) {
+            System.out.println(line);
+        }
+
         System.out.println("Bye. Hope to see you again!");
     }
 }

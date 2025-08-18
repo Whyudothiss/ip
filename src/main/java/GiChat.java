6da1d4b04b32c7a34b2abf5d1f91f2f5e8a6b762
Whyudothiss <weichenhow@gmail.com>
2025-08-18 23:44:32 +0800

    Renamed bot name and did initial skeleton
    Changed my bot name to GiChat and did the initial skeleton for the code as per requested

---
+++ src/main/java/GiChat.java
@@ -0,0 +1,6 @@
+public class GiChat {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm your GiChat \nWhat can I do for you");
+        System.out.println("Bye. Hope to see you again!");
+    }
+}

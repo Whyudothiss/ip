c006bc28f361a93298c12ee218672810572260cc
Whyudothiss <weichenhow@gmail.com>
2025-08-19 00:44:33 +0800

    added two methods for level 2
    added addTask() and listTasks() to add tasks and list the tasks, then also created an arraylist to track the tasks added using addTask, and also allow listTasks to display the task as intended

--- src/main/java/GiChat.java
+++ src/main/java/GiChat.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GiChat {
+    private static ArrayList<String> tasks = new ArrayList<>();
     public static void main(String[] args) {
         String border = "______________________________________________";
         System.out.println(border);
@@ -12,12 +15,37 @@ public class GiChat {
 
         // Scans the next line of input and check if the user says "bye"
         while (!(line = scanner.nextLine()).equals("bye")) {
-            System.out.println(border);
-            System.out.println(line);
-            System.out.println(border);
+            // Check if the user types out "List" if so call listTask else call addTask
+            if (line.equals("list")) {
+                listTasks(border);
+            } else {
+                addTask(line, border);
+            }
         }
         System.out.println(border);
         System.out.println("Bye. Hope to see you again!");
         System.out.println(border);
     }
+
+    // Used arraylist to track the task list so that it can be called at listTask()
+    private static void addTask(String task, String barrier) {
+        tasks.add(task);
+        System.out.println(barrier);
+        System.out.println("added: " + task);
+        System.out.println(barrier);
+    }
+    // First check if array is empty, if not run through array then print out the tasks
+    private static void listTasks(String barrier) {
+        System.out.println(barrier);
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in your list :)");
+            System.out.println(barrier);
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+            System.out.println(barrier);
+        }
+    }
+
 }

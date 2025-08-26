2f4e0e69e1bf1c0c17552bf84e08b8bfa7c654c0
Whyudothiss <weichenhow@gmail.com>
2025-08-26 20:47:27 +0800

    Create TaskList.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        if (index < 0 || index >= tasks.size()) {
+            throw new ArrayIndexOutOfBoundsException("Task Index out of bounds");
+        }
+        return tasks.remove(index);
+    }
+
+    public Task getTask(int index) {
+        if (index < 0 || index >= tasks.size()) {
+            throw new ArrayIndexOutOfBoundsException("Task Index out of bounds");
+        }
+        return tasks.get(index);
+    }
+
+    public int getSize() {
+        return this.tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public ArrayList<Task> getAlLTasks() {
+        return tasks;
+    }
+}
